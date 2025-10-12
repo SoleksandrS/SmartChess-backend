b3b74699d1a1831a765e2b467a1a369aecff355f
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-10-12 17:57:25 +0300

    Feat: add typeorm to app and deploy database via docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: true,
+  entities: ['src/modules/**/*.entity.ts'],
+  migrations: ['migrations/*.ts'],
+  migrationsTableName: 'migrations',
+});
