3a1b87fb5fbd5215769df5304f5b5fb5d5a04a6f
SOleksandrS <o.y.sitaylo@student.khai.edu>
2025-10-18 19:53:24 +0300

    Feat: add transaction to makeMove method of games service

--- src/modules/games/games.service.ts
+++ src/modules/games/games.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { constants } from 'src/config';
 import { EGameSide } from 'src/types/chess.types';
@@ -10,6 +10,7 @@ import { CreateGameDto } from './dto/create-game.dto';
 @Injectable()
 export class GamesService {
   constructor(
+    private dataSource: DataSource,
     @InjectRepository(Game)
     private gameRepo: Repository<Game>,
     @InjectRepository(GameMove)
@@ -41,9 +42,13 @@ export class GamesService {
   }
 
   async makeMove(id: number, turn: EGameSide, move: string) {
+    const qr = this.dataSource.createQueryRunner();
+    await qr.connect();
+    await qr.startTransaction();
+
     try {
       const params = { where: { id }, relations: ['moves'] };
-      const game = await this.gameRepo.findOne(params);
+      const game = await qr.manager.findOne(Game, params);
       if (!game)
         throw new HttpException('Game not found', HttpStatus.NOT_FOUND);
 
@@ -52,16 +57,22 @@ export class GamesService {
         turn === EGameSide.WHITE ? EGameSide.BLACK : EGameSide.WHITE;
 
       const body1 = { fen, turn: nextTurn };
-      await this.gameRepo.update(id, body1);
+      await qr.manager.update(Game, id, body1);
 
       const moveNumber = game.moves.length + 1;
       const body2 = { gameId: game.id, moveNumber, turn, move };
-      await this.moveRepo.insert(body2);
+      const entity = this.moveRepo.create(body2);
+      await qr.manager.save(entity);
+
+      await qr.commitTransaction();
 
       return true;
     } catch (err) {
+      await qr.rollbackTransaction();
       if (err instanceof HttpException) throw err;
       throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    } finally {
+      await qr.release();
     }
   }
 
